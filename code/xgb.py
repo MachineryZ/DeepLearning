4c2aca9dcf8f3720d257734e4409a1ba8bc327de
MachineryZ <zhizuozhang@gmail.com>
2023-01-10 15:27:08 +0800

    update xgb code

--- code/xgb.py
+++ code/xgb.py
@@ -1,8 +1,3 @@
-# Xgboost
-
-https://arxiv.org/pdf/1603.02754.pdf
-
-~~~python
 from xgboost import XGBRegressor as XGBR
 from sklearn.ensemble import RandomForestRegressor as RFR
 from sklearn.linear_model import LinearRegression as LinearR
@@ -22,7 +17,7 @@ y = data.target
 Xtrain, Xtest, Ytrain, Ytest = TTS(X, y, test_size=0.3, random_state=420)
 reg = XGBR(n_estimators=100).fit(Xtrain, Ytrain)
 reg.predict(Xtest)
-ret.score(Xtest, Ytest)
+reg.score(Xtest, Ytest)
 
 MSE(Ytest, reg.predict(Xtest))
 print(reg.feature_importances_)
@@ -45,25 +40,25 @@ CVS(reg, Xtrain, Ytrain, cv=5, scoring="neg_mean_squared_error").mean()
 
 def plot_learning_curve(estimator, title, X, y, ax=None, ylim=None, cv=None, n_jobs=None):
     from sklearn.model_selection import learning_curve
-    import matplotlib.pyplot.plt
+    import matplotlib.pyplot as plt
     import numpy as np
     train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, shuffle=True, cv=cv, n_jobs=n_jobs)
     if ax == None:
         ax = plt.gca()
-    eles:
+    else:
         ax = plt.figure()
     ax.set_title(title)
     if ylim is not None:
         ax.set_ylim(*ylim)
     ax.set_xlabel("Training examples")
-    ax.setylabel("Score")
+    ax.set_ylabel("Score")
     ax.grid()
     ax.plot(train_sizes, np.mean(train_scores, axis=1), "o-", color="r", label="Training score")
     ax.plot(train_sizes, np.mean(train_scores, axis=1), "o-", color="g", label="Test score")
     ax.legend(loc="best")
     return ax
 
-cv = KFold(n_split=5, shuffle=True, random_state=42)
+cv = KFold(n_splits=5, shuffle=True, random_state=42)
 plot_learning_curve(XGBR(n_estimators=100, random_state=420), "XGB", Xtrain, Ytrain, ax=None, cv=cv)
+plt.savefig("./xgb.png")
 plt.show()
-~~~
