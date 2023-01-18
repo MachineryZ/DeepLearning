38314905ef4a4404160411d6a8344525f61f6cb5
MachineryZ <zhizuozhang@gmail.com>
2023-01-18 14:54:55 +0800

    update ddpm code

--- code/ddpm.py
+++ code/ddpm.py
@@ -163,6 +163,57 @@ def plot(imgs, with_orig=False, row_title=None, **imshow_kwargs):
             axs[0, 0].title.set_size(8)
         if row_title is not None:
             for row_idx in range(num_rows):
-                continue
+                axs[row_idx, 0].set(ylabel=row_title[row_isx])
+        
+        plt.tight_layout()
+        plt.show()
+
+def p_losses(denoise_model, x_start, t, nois=None, loss_type="l1"):
+    if noise is None:
+        noise = torch.randn_like(x_start)
+    
+    x_noisy = q_sample(x_start=x_start, t=t, noise=noise)
+    predicted_noise = denoise_model(x_noisy, t)
+
+    if loss_type == "l1":
+        loss = F.l1_loss(noise, predicted_noise)
+    elif loss_type == "l2":
+        loss = F.mse_loss(noise, predicted_noise)
+    elif loss_type == "huber":
+        loss = F.smooth_l1_loss(noise, predicted_noise)
+    else:
+        raise NotImplementedError()
+
+    return loss
+
+
+def p_sample(model, x, t, t_index):
+    betas_t = extract(betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(sqrt_one_minus_alpha_cumprod, t, x.shape)
+    sqrt_recip_alphas_t = extract(sqrt_recip_alphas, t, x.shape)
+    model_mean = sqrt_recip_alphas_t * (x - betas_t * model(x, t) / sqrt_one_minus_alphas_cumprod_t)
+
+    if t_index == 0:
+        return model_mean
+    else:
+        posterior_variance_t = extract(posterior_variance, t, x.shape)
+        noise = torch.randn_like(x)
+        return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+def p_sample_loop(model, shape):
+    device = next(model.parameters()).device
+    b = shape[0]
+    img = torch.randn(shape, device = device)
+    imgs = []
+    for i in tqdm(reversed(range(0, timesteps)), desc="sampling loop time step", total=timesteps):
+        img = p_sample(model, img, torch.full((b,), i, device=device, dtype=torch.long), i)
+        imgs.append(img.cpu().numpy())
+    return imgs
+
+def sample(model, image_size, batch_size=16, channels=3):
+    return p_sample_loop(model, shape=(batch_size, channels, image_size, image_size))
+
+
+    
 
 
