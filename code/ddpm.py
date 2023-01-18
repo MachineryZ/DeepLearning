c3eade0495f24e990c5b9c1bec1bb5c9e86daa44
MachineryZ <zhizuozhang@gmail.com>
2023-01-18 14:06:59 +0800

    update ddpm code

--- code/ddpm.py
+++ code/ddpm.py
@@ -153,7 +153,16 @@ def plot(imgs, with_orig=False, row_title=None, **imshow_kwargs):
     fig, axs = plt.subplots(figsize=(200, 200), nrows=num_rows, ncols=num_cols, squeeze=False)
     for row_idx, row in enumerate(imgs):
         row = [image] + row if with_orig else row
+        for col_idx, img in enumerate(row):
+            ax = axs[row_idx, col_idx]
+            ax.imshow(np.asarray(img), **imshow_kwargs)
+            ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
-
+        if with_orig:
+            axs[0, 0].set(title="Original image")
+            axs[0, 0].title.set_size(8)
+        if row_title is not None:
+            for row_idx in range(num_rows):
+                
 
 
